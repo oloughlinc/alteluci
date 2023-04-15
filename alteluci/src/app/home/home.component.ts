9c5802cd08bfd45a1fea9f784aadb9f5911e9953
oloughlinc <craig.oloughlin@hotmail.com>
2023-04-15 19:33:15 -0400

    add menu and animations

--- alteluci/src/app/home/home.component.ts
+++ alteluci/src/app/home/home.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+//animation imports
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+
+  
+  // this is menu animation and all of this belongs in a menu componennt
+  animations: [
+    trigger('menuAnimation',
+    [
+      transition(':enter',
+        [
+          style({opacity: 0}),
+          animate('0.5s', style({opacity: 0.9}))
+        ]
+      ),
+      transition(':leave',
+        [
+          style({opacity: 0.9}),
+          animate('0.5s', style({opacity: 0}))
+        ]
+      ),
+    ]
+    )
+  ],
+  
+
 })
 export class HomeComponent {
+
+  showMenu = false;
+
+  public toggleMenu(){
+    this.showMenu = !this.showMenu;
+  }
 
 }
